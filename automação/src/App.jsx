91cd845d8a335bea8d749d12709e7c13fba96a50
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-09-04 18:30:47 -0300

    estudos em react
    eventos em react, hooks e automação front end

---
+++ automação/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = () => {
+  return <button>COMPRAR</button>
+}
+
+const App = () => {
+  return <div>
+    <a href="https://www.origamid.com">Origamid</a>
+    <Button />
+    </div>
+}
+
+export default App
