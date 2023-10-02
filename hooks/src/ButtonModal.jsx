6cd5e5d9db2ea2cf7c90e4fe547a065077efbba2
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-10-02 17:26:18 -0300

    estudo
    estudo do hook useState

---
+++ hooks/src/ButtonModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ButtonModal = ({setModal}) => {
+  return <button onClick={() => setModal(true)}>ABRIR</button>
+}
+
+export default ButtonModal
--- hooks/src/Estados.jsx
+++ hooks/src/Estados.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import ButtonModal from './ButtonModal'
+import Modal from './Modal.jsx'
 
 const Estados = () => {
-  let estado = false; // true = ativo; false = desativo;
+  const [estado, setEstado] = React.useState(true)
+
+  const [dados, setDados] = React.useState({nome: 'Emanuel', idade: 19})
+
+  const [modal, setModal] = React.useState(false)
 
   function handleClick(){
-    if(estado === true){
-      estado = false
-    }else{
-      estado = true
-    }
-    console.log(estado)
+    setEstado(!estado)
+    setDados({...dados, faculdade: 'Faculdade incompleta'})
   }
 
   return <div>
-    <button onClick={handleClick}>{estado?'ATIVO':'DESATIVO'}</button>
+    <button onClick={handleClick}>{estado?'ATIVO':'INATIVO'}</button>
+
+    <div>
+      <p>{dados.nome}</p>
+      <p>{dados.idade}</p>
+      <p>{dados.faculdade}</p>
+    </div>
+
+    <div>
+      <div>{modal?'MODAL É TRUE':'MODAL É FALSE'}</div>
+      <Modal modal={modal} setModal={setModal}/>
+      <ButtonModal modal={modal} setModal={setModal} />
+    </div>
   </div>
 }
 
