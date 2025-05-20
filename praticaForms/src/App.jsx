7ede393754c277ebbe5509320e445428606e9a75
Emanuel Victor Fontoura Cantalejo <emanuelfontoura03@gmail.com>
2025-05-19 22:38:45 -0300

    relembrando o react

--- praticaForms/src/App.jsx
+++ praticaForms/src/App.jsx
@@ -1,82 +1,117 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   const formFields = [
     {
-      type: "text",
-      label: "Nome",
-      id: "nome"
+      type: 'text',
+      label: 'Nome',
+      id: 'nome',
     },
     {
-      type: "password",
-      label: "Senha",
-      id: "senha"
-    }
-  ]
+      type: 'password',
+      label: 'Senha',
+      id: 'senha',
+    },
+  ];
 
-  const [form, setForm] = React.useState(formFields.reduce((acc, field) => {
-    return {...acc, [field.id]: ""}
-  }, {}))
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return { ...acc, [field.id]: '' };
+    }, {}),
+  );
 
-  const [msg, setMsg] = React.useState("")
-  const [select, setSelect] = React.useState("")
-  const [sexo, setSexo] = React.useState("")
+  const [msg, setMsg] = React.useState('');
+  const [select, setSelect] = React.useState('');
+  const [sexo, setSexo] = React.useState('');
 
-  function handleChange(event){
-    console.log(form)
-    const {id, value} = event.target
-    setForm({...form, [id]:value})
-    console.log(form)
+  function handleChange(event) {
+    console.log(form);
+    const { id, value } = event.target;
+    setForm({ ...form, [id]: value });
+    console.log(form);
   }
 
-  function handleSubmit(event){
-    event.preventDefault()
-    window.alert('ENVIADO!')
+  function handleSubmit(event) {
+    event.preventDefault();
+    window.alert('ENVIADO!');
   }
 
-  function handleChangeSexo(event){
-    setSexo(event.target.value)
+  function handleChangeSexo(event) {
+    setSexo(event.target.value);
   }
 
-  return <>
-    <form onSubmit={handleSubmit}>
-      {formFields.map(({type, label, id}) => {
-        return <div key={id}>
-          <label htmlFor={id}>{label}</label>
-          <input type={type} id={id} value={form[id]} onChange={handleChange}/>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        {formFields.map(({ type, label, id }) => {
+          return (
+            <div key={id}>
+              <label htmlFor={id}>{label}</label>
+              <input
+                type={type}
+                id={id}
+                value={form[id]}
+                onChange={handleChange}
+              />
+            </div>
+          );
+        })}
+
+        <div>
+          <label htmlFor="msg">Mensagem</label>
+          <textarea
+            name="msg"
+            id="msg"
+            cols="20"
+            rows="5"
+            value={msg}
+            onChange={({ target }) => setMsg(target.value)}
+          />
         </div>
-      })}
 
-      <div>
-        <label htmlFor="msg">Mensagem</label>
-        <textarea name="msg" id="msg" cols="20" rows="5" value={msg} onChange={({target}) => setMsg(target.value)}/>
-      </div>
+        <div>
+          <select
+            name="opcoes"
+            id="opcoes"
+            value={select}
+            onChange={({ target }) => setSelect(target.value)}
+          >
+            <option value="">Selecione uma opção</option>
+            <option value="notebook">Notebook</option>
+            <option value="tablet">Tablet</option>
+            <option value="smartphone">Smartphone</option>
+          </select>
+        </div>
 
-      <div>
-        <select name="opcoes" id="opcoes" value={select} onChange={({target}) => setSelect(target.value)}>
-          <option value="">Selecione uma opção</option>
-          <option value="notebook">Notebook</option>
-          <option value="tablet">Tablet</option>
-          <option value="smartphone">Smartphone</option>
-        </select>
-      </div>
+        <div>
+          <h1>Sexo</h1>
+          <label htmlFor="imasc">Masculino</label>
+          <input
+            type="radio"
+            name="sexo"
+            id="imasc"
+            value="M"
+            onChange={handleChangeSexo}
+          />
+          <label htmlFor="ifem">Feminino</label>
+          <input
+            type="radio"
+            name="sexo"
+            id="ifem"
+            value="F"
+            onChange={handleChangeSexo}
+          />
+        </div>
 
-      <div>
-        <h1>Sexo</h1>
-        <label htmlFor="imasc">Masculino</label>
-        <input type="radio" name="sexo" id="imasc" value="M" onChange={handleChangeSexo}/>
-        <label htmlFor="ifem">Feminino</label>
-        <input type="radio" name="sexo" id="ifem" value="F" onChange={handleChangeSexo}/>
-      </div>
+        <button type="submit">ENVIAR</button>
+      </form>
+      <p>Nome digitado: {form.nome}</p>
+      <p>Senha digitado: {form.senha}</p>
+      <p>Mensagem digitada: {msg}</p>
+      <p>Opção de produto selecionada: {select}</p>
+      <p>Sexo selecionado: {sexo}</p>
+    </>
+  );
+};
 
-      <button type="submit">ENVIAR</button>
-    </form>
-    <p>Nome digitado: {form.nome}</p>
-    <p>Senha digitado: {form.senha}</p>
-    <p>Mensagem digitada: {msg}</p>
-    <p>Opção de produto selecionada: {select}</p>
-    <p>Sexo selecionado: {sexo}</p>
-  </>
-}
-
-export default App
+export default App;
---
+++ revisando/aula01/src/App.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+function App() {
+  const formFields = [
+    {
+      text: 'Masculino',
+      type: 'radio',
+      id: 'imasc',
+      value: 'M',
+      name: 'sexo',
+    },
+    {
+      text: 'Feminino',
+      type: 'radio',
+      id: 'ifem',
+      value: 'F',
+      name: 'sexo',
+    },
+  ];
+
+  const [sexo, setSexo] = React.useState(
+    formFields.reduce((acc, field) => {
+      return { ...acc, [field.name]: '' };
+    }, {}),
+  );
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(`Sexo selecionado: ${sexo.sexo}`);
+  }
+
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setSexo({ ...sexo, [name]: value });
+    console.log(sexo);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+          <h1>Sexo</h1>
+          <div>
+            {formFields.map((field) => {
+              return (
+                <>
+                  <label htmlFor={field.id}>{field.text}</label>
+                  <input
+                    type={field.type}
+                    id={field.id}
+                    name={field.name}
+                    value={field.value}
+                    checked={sexo[field.name] === field.value}
+                    onChange={handleChange}
+                  />
+                </>
+              );
+            })}
+          </div>
+        </div>
+        <button type="submit">ENVIAR</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
